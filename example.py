12e041ab91ade9b115f42f2010043c3b70681b38
JohnnyGV <84827596+JohnnyGV@users.noreply.github.com>
2021-07-11 20:02:50 +1000

    Add files via upload

---
+++ example.py
@@ -0,0 +1,37 @@
+#import micropython
+#micropython.alloc_emergency_exception_buf(100)
+
+from machine import Pin
+from machine import Timer
+import sst
+from sst import Sst
+
+class Foo(object):
+    def __init__(self, timer_no, period, led_no):
+        self.led = Pin(led_no, Pin.OUT)
+        self.timer = Timer(timer_no)
+        self.task = 0
+        self.timer.init(period=period, mode=Timer.PERIODIC, callback=self.cb)
+        self.task_id = Sst.add_task(self.led_on_task)
+        
+    def cb(self, timer):
+        Sst.make_ready(self.task_id)
+
+    def led_on_task(self):
+        self.led.on()
+        Sst.set_task(self.led_off_task, self.task_id)
+
+    def led_off_task(self):
+        self.led.off()
+        Sst.set_task(self.led_on_task, self.task_id)
+
+def run():
+    red = Foo(1, 197, 18) 
+    Sst.make_ready(red.task_id)
+    green = Foo(2, 167, 19)
+    Sst.make_ready(green.task_id)
+    blue = Foo(3, 143, 23)
+    Sst.make_ready(blue.task_id)
+    Sst.start()
+ 
+run()
